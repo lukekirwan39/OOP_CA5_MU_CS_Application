ed60cdefb3359efa9ce3e1ec28fc2325b0da6ba1
lukekirwan39 <lukekirwan39@gmail.com>
2025-03-04 11:39:43 +0000

    Feature 4: insertWorkout method done.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,15 @@ import org.example.DTOs.WorkoutDTO;
 import org.example.Interfaces.WorkoutDAOInterface;
 
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try {
+
+            Scanner scanner = new Scanner(System.in);
 
             WorkoutDAOInterface wDAOInterface = new WorkoutDAO();
             List<WorkoutDTO> workouts = wDAOInterface.getAllWorkouts();
@@ -25,6 +29,22 @@ public class Main {
             System.out.println();
 
             wDAOInterface.deleteWorkout(10);
+
+            System.out.println();
+
+            System.out.println("Enter your User ID: ");
+            int id = scanner.nextInt();
+            System.out.print("Enter Workout Type: ");
+            String type = scanner.next();
+            System.out.print("Enter Duration: ");
+            int duration = scanner.nextInt();
+            System.out.print("Enter Calories Burned: ");
+            int caloriesBurned = scanner.nextInt();
+            System.out.print("Enter Workout Date (yyyy-mm-dd): ");
+            String workoutDate = scanner.next();
+            System.out.print("Enter Notes: ");
+            String notes = scanner.next();
+            wDAOInterface.insertWorkout(new WorkoutDTO(0, id, type, duration, caloriesBurned, java.sql.Date.valueOf(workoutDate), notes));
 
         } catch (SQLException e){
             throw new RuntimeException(e);
